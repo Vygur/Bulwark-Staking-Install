b454d13f669837d59c81d508c5a1c3f7696e3a88
Vygur <33483771+Vygur@users.noreply.github.com>
2018-09-26 01:17:58 -0600

    Update refresh.sh

--- refresh.sh
+++ refresh.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-BOOTSTRAPURL=$(curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep bootstrap.dat.xz | grep browser_download_url | cut -d '"' -f 4)
+BOOTSTRAPURL=$(curl -s https://api.github.com/repos/vulcanocrypto/vulcano/releases/latest | grep bootstrap.dat.xz | grep browser_download_url | cut -d '"' -f 4)
 BOOTSTRAPARCHIVE="bootstrap.dat.xz"
 
 # Make sure curl is installed
@@ -12,39 +12,39 @@ echo "This script will refresh your wallet."
 read -pr "Press Ctrl-C to abort or any other key to continue. " -n1 -s
 clear
 
-USER=bulwark
-USERHOME=/home/bulwark
+USER=vulcano
+USERHOME=/home/vulcano
 
-sudo systemctl stop bulwarkd
+sudo systemctl stop vulcanod
 
 echo "Refreshing node, please wait."
 
 sleep 5
 
-sudo rm -Rf "$USERHOME/.bulwark/blocks"
-sudo rm -Rf "$USERHOME/.bulwark/database"
-sudo rm -Rf "$USERHOME/.bulwark/chainstate"
-sudo rm -Rf "$USERHOME/.bulwark/peers.dat"
+sudo rm -Rf "$USERHOME/.vulcanocore/blocks"
+sudo rm -Rf "$USERHOME/.vulcanocore/database"
+sudo rm -Rf "$USERHOME/.vulcanocore/chainstate"
+sudo rm -Rf "$USERHOME/.vulcanocore/peers.dat"
 
-sudo cp "$USERHOME/.bulwark/bulwark.conf" "$USERHOME/.bulwark/bulwark.conf.backup"
-sudo sed -i '/^addnode/d' "$USERHOME/.bulwark/bulwark.conf"
+sudo cp "$USERHOME/.vulcanocore/vulcano.conf" "$USERHOME/.vulcanocore/vulcano.conf.backup"
+sudo sed -i '/^addnode/d' "$USERHOME/.vulcanocore/vulcano.conf"
 
 echo "Installing bootstrap file..."
-wget "$BOOTSTRAPURL" && sudo xz -cd $BOOTSTRAPARCHIVE && sudo mv "./bootstrap.dat"  "$USERHOME/.bulwark/bootstrap.dat" && rm $BOOTSTRAPARCHIVE
+wget "$BOOTSTRAPURL" && sudo xz -cd $BOOTSTRAPARCHIVE && sudo mv "./bootstrap.dat"  "$USERHOME/.vulcanocore/bootstrap.dat" && rm $BOOTSTRAPARCHIVE
 
-sudo systemctl start bulwarkd
+sudo systemctl start vulcanod
 
 clear
 
 echo "Your wallet is syncing. Please wait for this process to finish."
 echo "This can take up to a few hours. Do not close this window." && echo ""
 
-until [ -n "$(bulwark-cli getconnectioncount 2>/dev/null)"  ]; do
+until [ -n "$(vulcano-cli getconnectioncount 2>/dev/null)"  ]; do
   sleep 1
 done
 
-until su -c "bulwark-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null" "$USER"; do
-  echo -ne "Current block: ""$(sudo su -c "bulwark-cli getinfo" "$USER" | grep blocks | awk '{print $3}' | cut -d ',' -f 1)"'\r'
+until su -c "vulcano-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null" "$USER"; do
+  echo -ne "Current block: ""$(sudo su -c "vulcano-cli getinfo" "$USER" | grep blocks | awk '{print $3}' | cut -d ',' -f 1)"'\r'
   sleep 1
 done
 
