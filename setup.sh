f93ff60da3cebee3440d6a07cb56c272875faec7
Vygur <33483771+Vygur@users.noreply.github.com>
2018-09-26 01:23:05 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,12 +21,12 @@ sudo apt-get install -y curl
 
 # Set the correct download path
 
-VPSTARBALLURL=$(curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4)
-VPSTARBALLNAME=$(curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4 | cut -d "/" -f 9)
-SHNTARBALLURL=$(curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep browser_download_url | grep ARM | cut -d '"' -f 4)
-SHNTARBALLNAME=$(curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep browser_download_url | grep ARM | cut -d '"' -f 4 | cut -d "/" -f 9)
-BWKVERSION=$(curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep browser_download_url | grep ARM | cut -d '"' -f 4 | cut -d "/" -f 8)
-BOOTSTRAPURL=$(curl -s https://api.github.com/repos/bulwark-crypto/bulwark/releases/latest | grep bootstrap.dat.xz | grep browser_download_url | cut -d '"' -f 4)
+VPSTARBALLURL=$(curl -s https://api.github.com/repos/vulcanocrypto/vulcano/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4)
+VPSTARBALLNAME=$(curl -s https://api.github.com/repos/vulcanocrypto/vulcano/releases/latest | grep browser_download_url | grep linux64 | cut -d '"' -f 4 | cut -d "/" -f 9)
+SHNTARBALLURL=$(curl -s https://api.github.com/repos/vulcanocrypto/vulcano/releases/latest | grep browser_download_url | grep ARM | cut -d '"' -f 4)
+SHNTARBALLNAME=$(curl -s https://api.github.com/repos/vulcanocrypto/vulcano/releases/latest | grep browser_download_url | grep ARM | cut -d '"' -f 4 | cut -d "/" -f 9)
+VULCVERSION=$(curl -s https://api.github.com/repos/vulcanocrypto/vulcano/releases/latest | grep browser_download_url | grep ARM | cut -d '"' -f 4 | cut -d "/" -f 8)
+BOOTSTRAPURL=$(curl -s https://api.github.com/repos/vulcanocrypto/vulcano/releases/latest | grep bootstrap.dat.xz | grep browser_download_url | cut -d '"' -f 4)
 BOOTSTRAPARCHIVE="bootstrap.dat.xz"
 
 clear
@@ -39,13 +39,13 @@ choose are a minimum of 16 characters with upper and lower case as well as
 numbers and symbols to help protect against brute force attacks.
 
 Performing any acts not expressly provided by the script will render your 
-staking wallet incapable of the Bulwark team being able to provide tech 
+staking wallet incapable of the Vulcano team being able to provide tech 
 support. Additionally, maintenance and coin safety are the sole 
 responsibility of the user.
 
 If you do not expressly follow the script and the associated instructions, 
 there is a very real chance your coins will be rendered inaccessible. 
-Bulwark takes no responsibility for any coins that are lost or stolen.
+Vulcano takes no responsibility for any coins that are lost or stolen.
 
 EOL
 read -rp "Press Ctrl-C to abort or any other key to continue. " -n1 -s
@@ -62,26 +62,26 @@ sleep 2
 # Check for systemd
 sudo systemctl --version >/dev/null 2>&1 || { echo "systemd is required. Are you using Ubuntu 16.04?"  >&2; exit 1; }
 
-# Create a bulwark user
-sudo adduser bulwark --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password > /dev/null
+# Create a vulcano user
+sudo adduser vulcano --gecos "First Last,RoomNumber,WorkPhone,HomePhone" --disabled-password > /dev/null
 
 # Set cracklib to require secure passwords and force even root to use them
 sudo sed -i '/pam_cracklib.so/ s/retry=3 minlen=8 difok=3/retry=10 minlen=12 dcredit=0 ucredit=0 lcredit=0 ocredit=0 difok=3 reject_username enforce_for_root/g' /etc/pam.d/common-password
 
 clear
 
-# Ask for a password, confirm it, then set the permissions for user bulwark
-echo "Please enter a password for the bulwark user on your system."
+# Ask for a password, confirm it, then set the permissions for user vulcano
+echo "Please enter a password for the vulcano user on your system."
 echo "This user will be a sudoer - please make sure you understand the implications of this."
 echo "See https://en.wikipedia.org/wiki/Sudo for more information."
 echo ""
 echo "USE A STRONG PASSWORD AND KEEP IT IN A SAFE PLACE."
 echo -e "IF YOUR ACCOUNT GETS COMPROMISED, YOUR FUNDS CAN BE STOLEN!\\n"
 sleep 2
-until sudo passwd bulwark; do echo "Please try again."; done
+until sudo passwd vulcano; do echo "Please try again."; done
 
-# Now that bulwark has a password, the account can be a sudoer
-sudo usermod -aG sudo bulwark
+# Now that vulcano has a password, the account can be a sudoer
+sudo usermod -aG sudo vulcano
 
 clear 
 
@@ -127,33 +127,33 @@ fi
 
 echo "Downloading binaries..."
 if grep -q "ARMv7" /proc/cpuinfo; then
-  # Install Bulwark daemon for ARMv7 systems
+  # Install Vulcano daemon for ARMv7 systems
   wget "$SHNTARBALLURL"
-  tar -xzvf "$SHNTARBALLNAME" && mv bin "bulwark-$BWKVERSION"
+  tar -xzvf "$SHNTARBALLNAME" && mv bin "vulcano-$VULCVERSION"
   rm "$SHNTARBALLNAME"
 else
-  # Install Bulwark daemon for x86 systems
+  # Install Vulcano daemon for x86 systems
   wget "$VPSTARBALLURL"
-  tar -xzvf "$VPSTARBALLNAME" && mv bin "bulwark-$BWKVERSION"
+  tar -xzvf "$VPSTARBALLNAME" && mv bin "vulcano-$VULCVERSION"
   rm "$VPSTARBALLNAME"
 fi
 
 echo "Installing binaries..."
-sudo mv "./bulwark-$BWKVERSION/bulwarkd" /usr/local/bin
-sudo mv "./bulwark-$BWKVERSION/bulwark-cli" /usr/local/bin
-sudo mv "./bulwark-$BWKVERSION/bulwark-tx" /usr/local/bin
-rm -rf "bulwark-$BWKVERSION"
+sudo mv "./vulcano-$VULCVERSION/vulcanod" /usr/local/bin
+sudo mv "./vulcano-$VULCVERSION/vulcano-cli" /usr/local/bin
+sudo mv "./vulcano-$VULCVERSION/vulcano-tx" /usr/local/bin
+rm -rf "vulcano-$VULCVERSION"
 
-# Create .bulwark directory
-sudo mkdir "/home/bulwark/.bulwark"
+# Create .vulcanocore directory
+sudo mkdir "/home/vulcano/.vulcanocore"
 
 # Install bootstrap file
 echo "Installing bootstrap file..."
-sudo wget "$BOOTSTRAPURL" && sudo xz -d "$BOOTSTRAPARCHIVE" && sudo mv "bootstrap.dat" "/home/bulwark/.bulwark/"
+sudo wget "$BOOTSTRAPURL" && sudo xz -d "$BOOTSTRAPARCHIVE" && sudo mv "bootstrap.dat" "/home/vulcano/.vulcanocore/"
 echo "Creating configuration files..."
 
-# Create bulwark.conf
-sudo tee -a "/home/bulwark/.bulwark/bulwark.conf" &> /dev/null << EOL
+# Create vulcano.conf
+sudo tee -a "/home/vulcano/.vulcanocore/vulcano.conf" &> /dev/null << EOL
 rpcuser=${RPCUSER}
 rpcpassword=${RPCPASSWORD}
 rpcallowip=127.0.0.1
@@ -164,19 +164,19 @@ logtimestamps=1
 maxconnections=256
 staking=1
 EOL
-sudo chmod 0600 "/home/bulwark/.bulwark/bulwark.conf"
-sudo chown -R bulwark:bulwark "/home/bulwark/.bulwark"
+sudo chmod 0600 "/home/vulcano/.vulcanocore/vulcano.conf"
+sudo chown -R vulcano:vulcano "/home/vulcano/.vulcanocore"
 
-sudo tee -a /etc/systemd/system/bulwarkd.service &> /dev/null << EOL
+sudo tee -a /etc/systemd/system/vulcanod.service &> /dev/null << EOL
 [Unit]
-Description=Bulwarks's distributed currency daemon
+Description=Vulcanos's distributed currency daemon
 After=network.target
 [Service]
 Type=forking
-User=bulwark
-WorkingDirectory=/home/bulwark
-ExecStart=/usr/local/bin/bulwarkd -conf=/home/bulwark/.bulwark/bulwark.conf -datadir=/home/bulwark/.bulwark
-ExecStop=/usr/local/bin/bulwark-cli -conf=/home/bulwark/.bulwark/bulwark.conf -datadir=/home/bulwark/.bulwark stop
+User=vulcano
+WorkingDirectory=/home/vulcano
+ExecStart=/usr/local/bin/vulcanod -conf=/home/vulcano/.vulcanocore/vulcano.conf -datadir=/home/vulcano/.vulcanocore
+ExecStop=/usr/local/bin/vulcano-cli -conf=/home/vulcano/.vulcanocore/vulcano.conf -datadir=/home/vulcano/.vulcanocore stop
 Restart=on-failure
 RestartSec=1m
 StartLimitIntervalSec=5m
@@ -185,21 +185,21 @@ StartLimitBurst=3
 [Install]
 WantedBy=multi-user.target
 EOL
-sudo systemctl enable bulwarkd
-echo "Starting bulwarkd..."
-sudo systemctl start bulwarkd
+sudo systemctl enable vulcanod
+echo "Starting vulcanod..."
+sudo systemctl start vulcanod
 
-until sudo su -c "$(bulwark-cli getconnectioncount 2>/dev/null)" bulwark; do
+until sudo su -c "$(vulcano-cli getconnectioncount 2>/dev/null)" vulcano; do
   sleep 1
 done
 
-if ! sudo systemctl status bulwarkd | grep -q "active (running)"; then
-  echo "ERROR: Failed to start bulwarkd. Please contact support."
+if ! sudo systemctl status vulcanod | grep -q "active (running)"; then
+  echo "ERROR: Failed to start vulcanod. Please contact support."
   exit
 fi
 
 echo "Waiting for wallet to load..."
-until sudo su -c "bulwark-cli getinfo 2>/dev/null | grep -q 'version'" bulwark; do
+until sudo su -c "vulcano-cli getinfo 2>/dev/null | grep -q 'version'" vulcano; do
   sleep 1;
 done
 
@@ -209,34 +209,34 @@ echo "Your node has been set up, now setting up staking..."
 
 sleep 5
 
-# Ensure bulwarkd is active
-  if sudo systemctl is-active --quiet bulwarkd; then
-    sudo systemctl start bulwarkd
+# Ensure vulcanod is active
+  if sudo systemctl is-active --quiet vulcanod; then
+    sudo systemctl start vulcanod
 fi
 echo "Setting Up Staking Address.."
 
-# Check to make sure the bulwarkd sync process is finished, so it isn't interrupted and forced to start over later.'
-echo "The script will begin set up staking once bulwarkd has finished syncing. Please allow this process to finish."
-until sudo su -c "bulwark-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null" bulwark; do
-  echo -ne "Current block: ""$(sudo su -c "bulwark-cli getinfo" bulwark | grep blocks | awk '{print $3}' | cut -d ',' -f 1)"'\r'
+# Check to make sure the vulcanod sync process is finished, so it isn't interrupted and forced to start over later.'
+echo "The script will begin set up staking once vulcanod has finished syncing. Please allow this process to finish."
+until sudo su -c "vulcano-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null" vulcano; do
+  echo -ne "Current block: ""$(sudo su -c "vulcano-cli getinfo" vulcano | grep blocks | awk '{print $3}' | cut -d ',' -f 1)"'\r'
   sleep 1
 done
 
 # Ensure the .conf exists
-sudo touch "/home/bulwark/.bulwark/bulwark.conf"
+sudo touch "/home/vulcano/.vulcanocore/vulcano.conf"
 
-# If the line does not already exist, adds a line to bulwark.conf to instruct the wallet to stake
+# If the line does not already exist, adds a line to vulcano.conf to instruct the wallet to stake
 
-sudo sed -i 's/staking=0/staking=1/' "/home/bulwark/.bulwark/bulwark.conf"
+sudo sed -i 's/staking=0/staking=1/' "/home/vulcano/.vulcanocore/vulcano.conf"
 
-if sudo grep -Fxq "staking=1" "/home/bulwark/.bulwark/bulwark.conf"; then
+if sudo grep -Fxq "staking=1" "/home/vulcano/.vulcanocore/vulcano.conf"; then
     echo "Staking Already Active"
   else
-    echo "staking=1" | sudo tee -a "/home/bulwark/.bulwark/bulwark.conf"
+    echo "staking=1" | sudo tee -a "/home/vulcano/.vulcanocore/vulcano.conf"
 fi
 
 # Generate new address and assign it a variable
-STAKINGADDRESS=$(sudo su -c "bulwark-cli getnewaddress" bulwark)
+STAKINGADDRESS=$(sudo su -c "vulcano-cli getnewaddress" vulcano)
 
 # Ask for a password and apply it to a variable and confirm it.
 ENCRYPTIONKEY=1
@@ -254,62 +254,62 @@ until [ "$ENCRYPTIONKEY" = "$ENCRYPTIONKEYCONF" ]; do
 done
 
 # Encrypt the new address with the requested password
-BIP38=$(sudo su -c "bulwark-cli bip38encrypt $STAKINGADDRESS '$ENCRYPTIONKEY'" bulwark)
+BIP38=$(sudo su -c "vulcano-cli bip38encrypt $STAKINGADDRESS '$ENCRYPTIONKEY'" vulcano)
 echo "Address successfully encrypted! Please wait for encryption to finish..."
 
 # Encrypt the wallet with the same password
-sudo su -c "bulwark-cli encryptwallet '$ENCRYPTIONKEY'" bulwark && echo "Wallet successfully encrypted!"
+sudo su -c "vulcano-cli encryptwallet '$ENCRYPTIONKEY'" vulcano && echo "Wallet successfully encrypted!"
 
-# Wait for bulwarkd to close down after wallet encryption
-echo "Waiting for bulwarkd to restart..."
-until  ! sudo systemctl is-active --quiet bulwarkd; do sleep 1; done
+# Wait for vulcanod to close down after wallet encryption
+echo "Waiting for vulcanod to restart..."
+until  ! sudo systemctl is-active --quiet vulcanod; do sleep 1; done
 
-# Open up bulwarkd again
-sudo systemctl start bulwarkd
+# Open up vulcanod again
+sudo systemctl start vulcanod
 
-# Wait for bulwarkd to open up again
-until sudo su -c "bulwark-cli getinfo &> /dev/null" bulwark; do sleep 1; done
+# Wait for vulcanod to open up again
+until sudo su -c "vulcano-cli getinfo &> /dev/null" vulcano; do sleep 1; done
 
 # Unlock the wallet for a long time period
-sudo su -c "bulwark-cli walletpassphrase '$ENCRYPTIONKEY' 0 true" bulwark
+sudo su -c "vulcano-cli walletpassphrase '$ENCRYPTIONKEY' 0 true" vulcano
 
 # Create decrypt.sh and service
 
 #Check if it already exists, remove if so.
-if [  -e /usr/local/bin/bulwark-decrypt ]; then sudo rm /usr/local/bin/bulwark-decrypt; fi
+if [  -e /usr/local/bin/vulcano-decrypt ]; then sudo rm /usr/local/bin/vulcano-decrypt; fi
 
 #create decrypt.sh
-sudo tee &> /dev/null /usr/local/bin/bulwark-decrypt << EOL
+sudo tee &> /dev/null /usr/local/bin/vulcano-decrypt << EOL
 #!/bin/bash
 
 # Stop writing to history
 set +o history
 
 # Confirm wallet is synced
-until sudo su -c "bulwark-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null" bulwark; do
-  echo -ne "Current block: "$(sudo su -c "bulwark-cli getinfo | grep blocks | awk '{print $3}' | cut -d ',' -f 1)'\\r'") bulwark
+until sudo su -c "vulcano-cli mnsync status 2>/dev/null | grep '\"IsBlockchainSynced\" : true' > /dev/null" vulcano; do
+  echo -ne "Current block: "$(sudo su -c "vulcano-cli getinfo | grep blocks | awk '{print $3}' | cut -d ',' -f 1)'\\r'") vulcano
   sleep 1
 done
 
 # Unlock wallet
-until sudo su -c "bulwark-cli getstakingstatus | grep walletunlocked | grep true" bulwark; do
+until sudo su -c "vulcano-cli getstakingstatus | grep walletunlocked | grep true" vulcano; do
 
   #ask for password and attempt it
   read -e -s -p "Please enter a password to decrypt your staking wallet. Your password will not show as you type : " ENCRYPTIONKEY
-  sudo su -c "bulwark-cli walletpassphrase '$ENCRYPTIONKEY' 0 true" bulwark
+  sudo su -c "vulcano-cli walletpassphrase '$ENCRYPTIONKEY' 0 true" vulcano
 done
 
 # Tell user all was successful
 echo "Wallet successfully unlocked!"
 echo " "
-sudo su -c "bulwark-cli getstakingstatus" bulwark
+sudo su -c "vulcano-cli getstakingstatus" vulcano
 
 # Restart history
 set -o history
 EOL
 
-sudo chmod a+x /usr/local/bin/bulwark-decrypt
-sudo chown -R bulwark:bulwark "/home/bulwark/.bulwark/"
+sudo chmod a+x /usr/local/bin/vulcano-decrypt
+sudo chown -R vulcano:vulcano "/home/vulcano/.vulcanocore/"
 
 cat << EOL
 Your wallet has now been set up for staking, please send the coins you wish to
@@ -317,7 +317,7 @@ stake to ${STAKINGADDRESS}. Once your wallet is synced your coins should begin
 staking automatically.
 
 To check on the status of your staked coins you can run
-"bulwark-cli getstakingstatus" and "bulwark-cli getinfo".
+"vulcano-cli getstakingstatus" and "vulcano-cli getinfo".
 
 You can import the private key for this address in to your QT wallet using
 the BIP38 tool under settings, just enter the information below with the
@@ -326,12 +326,12 @@ lines to assist with recovery if ever needed.
 
 ${BIP38}
 
-If your bulwarkd restarts, and you need to unlock your wallet again, use
-the included script by typing "bulwark-decrypt" to unlock your wallet securely.
+If your vulcanod restarts, and you need to unlock your wallet again, use
+the included script by typing "vulcano-decrypt" to unlock your wallet securely.
 
 After the installation script ends, we will wipe all history and have no
 storage record of your password, encrypted key, or addresses.
-Any funds you lose access to are your own responsibility and the Bulwark team
+Any funds you lose access to are your own responsibility and the Vulcano team
 will be unable to assist with their recovery. We therefore suggesting saving a
 physical copy of this information.
 
@@ -347,7 +347,7 @@ until [  "$CONFIRMATION" = "I have read the above and agree"  ]; do
     safe by typing \"I have read the above and agree\" : " CONFIRMATION
 done
 
-echo "Thank you for installing your Bulwark staking wallet!"
+echo "Thank you for installing your Vulcano staking wallet!"
 
 unset CONFIRMATION ENCRYPTIONKEYCONF ENCRYPTIONKEY 
 
@@ -404,7 +404,7 @@ EOL
 # Harden sshd_config
 echo "Hardening sshd_config..."
 sudo tee /etc/ssh/sshd_config &> /dev/null << EOL
-# Bulwark-Staking-Install generated configuration file
+# Vulcano-Staking-Install generated configuration file
 # See the sshd_config(5) manpage for details
 
 # What ports, IPs and protocols we listen for
@@ -519,14 +519,14 @@ sudo apt install -y unattended-upgrades &> /dev/null
 clear
 echo "Hardening complete."
 sleep 2
-echo "Shutting down bulwarkd..."
-sudo systemctl stop bulwarkd
+echo "Shutting down vulcanod..."
+sudo systemctl stop vulcanod
 clear
 cat << EOL
 PLEASE NOTE:
 
 After the system reboots, you can no longer log in with the root user. You will need to
-log in with the bulwark user and the password you set earlier.
+log in with the vulcano user and the password you set earlier.
 
 The first time you try to log in after the reboot, you will get the following error:
 
